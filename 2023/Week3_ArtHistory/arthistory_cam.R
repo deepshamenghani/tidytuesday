7ad45e19617f0e63046ba664f03ae7c01b2c186f
deepshamenghani <46545400+deepshamenghani@users.noreply.github.com>
2023-01-21 14:35:59 -0800

    Updated colors

--- 2023/Week3_ArtHistory/arthistory_cam.R
+++ 2023/Week3_ArtHistory/arthistory_cam.R
@@ -5,6 +5,17 @@ library(cowplot)
 library(magick)
 library(ggimage)
 
+Gardnerimage <- image_read("GardnersATTP2.png")
+Gardnerimage_transparent <- image_colorize(Gardnerimage, opacity = 60, color = "white")
+Gardnerimage_transparent2 <- image_colorize(Gardnerimage, opacity = 20, color = "white")
+
+
+ggdraw() +
+  draw_plot(wednesday_chart) +
+  draw_image(wednesdayadams2, x = 0.9, y = 0.9, hjust = 1, vjust = 1, width = 0.13, height = 0.4) 
+
+artists <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-17/artists.csv')
+
 gg_record(
   dir = file.path("recording"), # where to save the recording
   device = "png", # device to use to save images
@@ -14,19 +25,24 @@ gg_record(
   dpi = 300 # dpi to use when saving image
 )
 
+
 artist_space_plot <-  artist_spaces %>%
+  mutate(label = if_else(year == max(year), as.character(artist_gender_race), NA_character_))%>% 
+  mutate(label = label %>% str_replace_all("White","W"))  %>% 
+  mutate(label_year = if_else(year %in% c(1936, 1959, 1986, 2016), as.character(year), NA_character_)) %>% 
+  filter((book %in% c("Gardner"))) %>% 
   ggplot(aes(year, maxspace ,color = artist_gender_race)) +
   geom_line(size = 2) +
   geom_text(aes(label = label),
             na.rm = TRUE,
-            size = 15,
+            size = 17,
             hjust = -0.15,
-            vjust=0.5, family = "MedievalSharp-Regular.ttf") +
+            vjust=0.5) +
   theme_minimal() +
   theme(
     panel.grid = element_blank()
   ) +
-  scale_color_manual(values = c("#8c684e", "gray18", "#337b70", "yellow4")) +
+  scale_color_manual(values = c("#8c684e", "gray18", "#337b70", "darkgreen")) +
   scale_y_continuous(limits = c(0, 3.5), breaks = seq(0, 3.5, by = 0.5)) +
   scale_x_continuous(limits = c(1926, 2045), breaks = seq(1926, 2045, by = 30))  +
   labs(
